91d15710eb31fc523898cbee48fad2912273d845
Wisam Fawzi <wisam.fawzi@devfacto.com>
2015-03-02 15:14:38 -0600

    Moved the turtle

---
+++ source/Engine.Tests/TurtleTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Tests
+{
+    [TestClass]
+    public class TurtleTests
+    {
+        [TestMethod]
+        public void Turtle_WhenCreated_StartsAtOrigin()
+        {
+            //Arrange
+            
+            //Act
+            var turtle = new Turtle();
+
+            //Assert
+            Assert.AreEqual(0.0, turtle.X, Double.Epsilon);
+            Assert.AreEqual(0.0, turtle.Y, Double.Epsilon);
+        }
+
+        [TestMethod]
+        public void Turtle_WhenCreatedWithPosition_StartsAtDefinedPosition()
+        {
+            //Arrange
+            var xPos = 3.0d;
+            var yPos = 3.0d;
+            
+            //Act
+            var turtle = new Turtle(xPos, yPos);
+
+            //Assert
+            Assert.AreEqual(xPos, turtle.X, Double.Epsilon);
+            Assert.AreEqual(yPos, turtle.Y, Double.Epsilon);
+        }
+
+        [TestMethod]
+        public void Turtle_MovedRight()
+        {
+            //Arrange
+            var xPos = 3.0d;
+            var yPos = 3.0d;
+
+            var step = 3.0d;
+
+            //Act
+            var turtle = new Turtle(xPos, yPos);
+            turtle.moveRight(step);
+
+            //Assert
+            Assert.AreEqual((xPos+step), turtle.X, Double.Epsilon);
+            Assert.AreEqual(yPos, turtle.Y, Double.Epsilon);
+        }
+
+        [TestMethod]
+        public void Turtle_MovedLeft()
+        {
+            //Arrange
+            var xPos = 3.0d;
+            var yPos = 3.0d;
+
+            var step = 3.0d;
+
+            //Act
+            var turtle = new Turtle(xPos, yPos);
+            turtle.moveLeft(step);
+
+            //Assert
+            Assert.AreEqual((xPos - step), turtle.X, Double.Epsilon);
+            Assert.AreEqual(yPos, turtle.Y, Double.Epsilon);
+        }
+    }
+}
+
+
+//When modifying the core logic, make sure that you do not break any of the older test
---
+++ source/Engine/Turtle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Turtle
+    {
+        public Turtle()
+        {
+            this.X = 0;
+            this.Y = 0;
+        }
+
+        public Turtle(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+
+        public void moveRight(double step)
+        {
+            X += step;
+        }
+
+        public void moveLeft(double step)
+        {
+            X -= step;
+        }
+
+        public void moveUp(double step)
+        {
+            Y -= step;
+        }
+
+        public void moveDown(double step)
+        {
+            Y += step;
+        }
+    }
+}
