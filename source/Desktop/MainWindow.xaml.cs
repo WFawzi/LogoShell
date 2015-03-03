727c661b2359548b30ec3ca43d7cfe359020e976
Wisam Fawzi <wisam.fawzi@devfacto.com>
2015-03-02 18:24:44 -0600

    Basic turtle drawing! It's an octagon!

--- source/Desktop/MainWindow.xaml.cs
+++ source/Desktop/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,12 +25,58 @@ namespace Logo
         {
             InitializeComponent();
 
-            var line = new Polyline();
-            line.Points.Add(new Point(0,0));
-            line.Points.Add(new Point(800,800));
-            line.StrokeThickness = 3;
-            line.Stroke = Brushes.Green;
-            LogoCanvas.Children.Add(line);
+            //var size = 10;
+            //var square = new Polygon();
+            //square.Stroke = Brushes.Green;
+            //square.Points.Add(new Point(20, 20));
+            //square.Points.Add(new Point(20+size/2, 20+size/2));
+            //square.Points.Add(new Point(20-size/2, 20+size/2));
+            //square.RenderTransformOrigin = new Point(20, 20);
+            //square.RenderTransform = new RotateTransform(90);
+            
+            
+            //square.Fill = Brushes.Green;
+            //LogoCanvas.Children.Add(square);
+            
+            //var line = new Polyline();
+            //line.Points.Add(new Point(0,0));
+            //line.Points.Add(new Point(800,800));
+            //line.StrokeThickness = 3;
+            //line.Stroke = Brushes.Green;
+            //LogoCanvas.Children.Add(line);
+
+            var turtle = new Turtle(200, 200);
+
+            var path = new Polyline();
+
+            path.Points.Add(new Point(turtle.X, turtle.Y));
+
+            turtle.MoveForward(100);
+            path.Points.Add(new Point(turtle.X, turtle.Y));
+            turtle.Turn(45);
+            turtle.MoveForward(100);
+            path.Points.Add(new Point(turtle.X, turtle.Y));
+            turtle.Turn(45);
+            turtle.MoveForward(100);
+            path.Points.Add(new Point(turtle.X, turtle.Y));
+            turtle.Turn(45);
+            turtle.MoveForward(100);
+            path.Points.Add(new Point(turtle.X, turtle.Y));
+            turtle.Turn(45);
+            turtle.MoveForward(100);
+            path.Points.Add(new Point(turtle.X, turtle.Y));
+            turtle.Turn(45);
+            turtle.MoveForward(100);
+            path.Points.Add(new Point(turtle.X, turtle.Y));
+            turtle.Turn(45);
+            turtle.MoveForward(100);
+            path.Points.Add(new Point(turtle.X, turtle.Y));
+            turtle.Turn(45);
+            turtle.MoveForward(100);
+            path.Points.Add(new Point(turtle.X, turtle.Y));
+            path.StrokeThickness = 2;
+            path.Stroke = Brushes.Black;
+            LogoCanvas.Children.Add(path);
         }
     }
 }
