8803b3734efdd9f1b831685ab00600b4bf4c84bc
Wisam Fawzi <wisam.fawzi@devfacto.com>
2015-03-03 14:33:37 -0600

    Added PowerShell Scripting Functionality

--- source/Desktop/MainWindow.xaml.cs
+++ source/Desktop/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Engine;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,56 +27,53 @@ namespace Logo
         public MainWindow()
         {
             InitializeComponent();
+            UserScriptBox.Text =
+@"
+#You can access the cursor using $input.
+#Move the cursor forward by pixels with $input.MoveForward(pixelCount)
+#Turn the cursor by degress with $input.Turn(degrees)
 
-            //var size = 10;
-            //var square = new Polygon();
-            //square.Stroke = Brushes.Green;
-            //square.Points.Add(new Point(20, 20));
-            //square.Points.Add(new Point(20+size/2, 20+size/2));
-            //square.Points.Add(new Point(20-size/2, 20+size/2));
-            //square.RenderTransformOrigin = new Point(20, 20);
-            //square.RenderTransform = new RotateTransform(90);
-            
-            
-            //square.Fill = Brushes.Green;
-            //LogoCanvas.Children.Add(square);
-            
-            //var line = new Polyline();
-            //line.Points.Add(new Point(0,0));
-            //line.Points.Add(new Point(800,800));
-            //line.StrokeThickness = 3;
-            //line.Stroke = Brushes.Green;
-            //LogoCanvas.Children.Add(line);
+$input.MoveForward(100);
+$input.Turn(90);
+$input.MoveForward(100);
+";
+        }
 
-            var turtle = new Turtle(200, 200);
+        private void ExecuteDrawing(object sender, RoutedEventArgs e)
+        {
+            var width = LogoCanvas.ActualWidth;
+            var height = LogoCanvas.ActualHeight;
+
+
+            var turtle = new Turtle(width / 2, height / 2);
+            var turtleVariable = new SessionStateVariableEntry("$turtle", turtle, "A tasty base for soup.");
+
+            var iss = InitialSessionState.Create();
+            iss.Variables.Add(turtleVariable);
+            iss.LanguageMode = PSLanguageMode.FullLanguage;
+
+            using (var myRunSpace = RunspaceFactory.CreateRunspace(iss))
+            {
+                myRunSpace.Open();
+                using (var powershell = PowerShell.Create())
+                {
+                    powershell.Runspace = myRunSpace;
+
+                    foreach(var line in UserScriptBox.Text.Split('\n'))
+                    {
+                        powershell.AddStatement().AddScript(line, true);
+                    }
+
+                    powershell.Invoke(new[] { turtle });
+                }
+            }
 
             var path = new Polyline();
 
-            path.Points.Add(new Point(turtle.X, turtle.Y));
-
-            turtle.MoveForward(100);
-            path.Points.Add(new Point(turtle.X, turtle.Y));
-            turtle.Turn(45);
-            turtle.MoveForward(100);
-            path.Points.Add(new Point(turtle.X, turtle.Y));
-            turtle.Turn(45);
-            turtle.MoveForward(100);
-            path.Points.Add(new Point(turtle.X, turtle.Y));
-            turtle.Turn(45);
-            turtle.MoveForward(100);
-            path.Points.Add(new Point(turtle.X, turtle.Y));
-            turtle.Turn(45);
-            turtle.MoveForward(100);
-            path.Points.Add(new Point(turtle.X, turtle.Y));
-            turtle.Turn(45);
-            turtle.MoveForward(100);
-            path.Points.Add(new Point(turtle.X, turtle.Y));
-            turtle.Turn(45);
-            turtle.MoveForward(100);
-            path.Points.Add(new Point(turtle.X, turtle.Y));
-            turtle.Turn(45);
-            turtle.MoveForward(100);
-            path.Points.Add(new Point(turtle.X, turtle.Y));
+            foreach (var point in turtle.Path)
+            {
+                path.Points.Add(new Point(point.X, point.Y));
+            }
             path.StrokeThickness = 2;
             path.Stroke = Brushes.Black;
             LogoCanvas.Children.Add(path);
