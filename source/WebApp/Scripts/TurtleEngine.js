df67c022af9d0935015b3cd7888caa6c2fe5ec82
Wisam Fawzi <wisam.fawzi@devfacto.com>
2015-03-06 12:22:37 -0600

    Turtle draws on the canvas

--- source/WebApp/Scripts/TurtleEngine.js
+++ source/WebApp/Scripts/TurtleEngine.js
@@ -1,17 +1,11 @@
-﻿$(document).ready(function () {
-    /*
-    var c = document.getElementById("logoCanvas");
-    var ctx = c.getContext("2d");
-    ctx.lineWidth = 2;
-    ctx.moveTo(0, 0);
-    ctx.lineTo(50, 150);
-    ctx.stroke();
-    */
-});
-
+﻿var turtleState = {
+    x: 0,
+    y: 0,
+    direction: 0
+};
 
 function SendScript() {
-    
+
     //Generally, the intlliesnece of JavaScript is not complete, so debug in the browser, to get the full intlliesnece
 
     //var userScript = document.getElementById("codeEditor").value; // Here we are getting the element from teh DOM, we can also use JQuery to get the element: var userScript = $('#codeEditor').value
@@ -19,37 +13,56 @@ function SendScript() {
 
     var formData = {
         userScript: userScript, //pass userScript to the UserScript of the DrawModel - the MVC model binder is not case sensitive, so "userScript:" here maps to UserScript of the DrawModel
-        direction: 0,
-        x: 0,
-        y: 0
+        direction: turtleState.direction,
+        x: turtleState.x,
+        y: turtleState.y
     }; //always use ";" in JS
- 
+
 
 
     $.ajax({
         url: "/Turtle/Draw",            //Remember from RouteConfig.cs, url: "{controller}/{action}
         type: "POST",                   //We are POSTing (writing to server) to the controller at the URL "/Turtle/Draw"
-        data : formData,
-        success: function(data, textStatus, jqXHR)
-        {
+        data: formData,
+        success: function (data, textStatus, jqXHR) {
             //data - response from server
-            debugger;
-            Draw();
+            Draw(data.x, data.y, data.direction, data.path);
         },
-        error: function (jqXHR, textStatus, errorThrown)
-        {
- 
+        error: function (jqXHR, textStatus, errorThrown) {
+
         }
     });
 }
 
 
-function Draw()
-{
+function Draw(x, y, direction, path) {
     var c = document.getElementById("logoCanvas");
+    //var c = $('#logoCanvas'); //<-- Not working with the below properties since the DOM and JQuery have different properties, "getContext" foe example is not part of the JQuery properties
+
+    var canvHeight = c.height;
+    var canvWidth = c.width;
+
     var ctx = c.getContext("2d");
     ctx.lineWidth = 2;
-    ctx.moveTo(0, 0);
-    ctx.lineTo(50, 150);
+
+
+    //moveTo, is where you start your drawing - the initial position of the pencil
+    //lineTo: is teh actual drawing
+    ctx.moveTo(((canvWidth / 2) + path[0].x), ((canvHeight / 2) -path[0].y));
+
+    for(var i = 1; i < path.length; i++){
+        var point = path[i];
+        var canvasX = (canvWidth / 2) + point.x;
+        var canvasY = (canvHeight / 2) - point.y;
+
+        ctx.lineTo(canvasX, canvasY);
+    };
+
     ctx.stroke();
+
+
+    turtleState.x = x;
+    turtleState.y = y;
+    turtleState.direction = direction;
+
 }
