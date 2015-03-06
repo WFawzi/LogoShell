992b905841b065456c43718d9aee2f0e83af473d
Wisam Fawzi <wisam.fawzi@devfacto.com>
2015-03-06 11:36:11 -0600

    Pass the turtle object from the server to the client

--- source/WebApp/Controllers/TurtleController.cs
+++ source/WebApp/Controllers/TurtleController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApp.Models;
+using Engine; //You have to add it through Refrences -> add refernce
+
 
 namespace WebApp.Controllers
 {
@@ -19,11 +21,27 @@ namespace WebApp.Controllers
 
 
         // POST: /Turtle/Draw
-        [HttpPost]
+        [HttpPost]          //We have to add this, so that the client can POST to this server method, if we do not add it, then the default behavior is GET
         public ActionResult Draw(DrawModel model)
         {
             string userScript = model.UserScript;
-            return Json(new { success = true });
+
+            //
+            // Passs the direction, X, Y and user script to the Engine
+            // Gets a turtle from the engine
+
+            Turtle turtle = PowerShellEnvironment.ExecuteTurtleScript(model.X, model.Y, model.Direction, model.UserScript);
+            //
+
+
+            //return Json(new { success = true });
+            return Json(new
+            {
+                direction = turtle.Direction,
+                x = turtle.Position.X,
+                y = turtle.Position.Y,
+                path = turtle.Path.Select(p => new { x = p.X, y = p.Y })
+            });
         }
 
     }
--- source/WebApp/Scripts/TurtleEngine.js
+++ source/WebApp/Scripts/TurtleEngine.js
@@ -1,22 +1,24 @@
 ﻿$(document).ready(function () {
-
+    /*
     var c = document.getElementById("logoCanvas");
     var ctx = c.getContext("2d");
     ctx.lineWidth = 2;
     ctx.moveTo(0, 0);
     ctx.lineTo(50, 150);
     ctx.stroke();
+    */
 });
 
 
 function SendScript() {
     
-    //var formData = { name: "ravi", age: "31" }; //Json
+    //Generally, the intlliesnece of JavaScript is not complete, so debug in the browser, to get the full intlliesnece
 
+    //var userScript = document.getElementById("codeEditor").value; // Here we are getting the element from teh DOM, we can also use JQuery to get the element: var userScript = $('#codeEditor').value
+    var userScript = $('#codeEditor').val(); //the JS can find the element with ID: codeEditor, because both the script and the element with ID: codeEditor or loaded on the same page
 
-    var userScript = document.getElementById("codeEditor").textContent;
     var formData = {
-        userScript: userScript,
+        userScript: userScript, //pass userScript to the UserScript of the DrawModel - the MVC model binder is not case sensitive, so "userScript:" here maps to UserScript of the DrawModel
         direction: 0,
         x: 0,
         y: 0
@@ -25,17 +27,29 @@ function SendScript() {
 
 
     $.ajax({
-        url: "/Turtle/Draw",
-        type: "POST",
+        url: "/Turtle/Draw",            //Remember from RouteConfig.cs, url: "{controller}/{action}
+        type: "POST",                   //We are POSTing (writing to server) to the controller at the URL "/Turtle/Draw"
         data : formData,
         success: function(data, textStatus, jqXHR)
         {
             //data - response from server
             debugger;
+            Draw();
         },
         error: function (jqXHR, textStatus, errorThrown)
         {
  
         }
     });
+}
+
+
+function Draw()
+{
+    var c = document.getElementById("logoCanvas");
+    var ctx = c.getContext("2d");
+    ctx.lineWidth = 2;
+    ctx.moveTo(0, 0);
+    ctx.lineTo(50, 150);
+    ctx.stroke();
 }
