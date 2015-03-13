f7a9d965dafd5987b238dd0b6764bf38e6649990
Wisam Fawzi <wisam.fawzi@devfacto.com>
2015-03-12 21:17:49 -0600

    Add  import user script functionality - WebApp

---
+++ source/WebApp/Scripts/importScript.js
@@ -0,0 +1,26 @@
+﻿var scriptFileContent = "";
+
+function readScript(evt) {
+    //Retrieve the first (and only!) File from the FileList object
+    var f = evt.target.files[0];
+
+    if (f) {
+        var r = new FileReader();
+        r.onload = function (e) {
+            var contents = e.target.result;
+            scriptFileContent = contents;
+        }
+        r.readAsText(f);
+    } else {
+        alert("Failed to load file");
+    }
+}
+
+document.getElementById('fileinput').addEventListener('change', readScript, false);
+
+
+function ImportScript()
+{
+    var userScript = document.getElementById("codeEditor");
+    userScript.value = scriptFileContent;
+}
